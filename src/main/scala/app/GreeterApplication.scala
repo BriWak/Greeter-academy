a9d4f854932fc2962a40665bb669d6db90075372
Brian Wakenshaw <bri.wakey@hotmail.co.uk>
2018-02-14 09:13:43 +0000

    Refactored Classes into Packages

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,65 +1,29 @@
 package app
 
-import scala.io.StdIn
+import app.models.{CashISASavingsAccount, Person}
+import app.views.Prompt
 
-object Prompt {
-  def ask(message : String) = StdIn.readLine(message)
-}
 
-class Person(name : String, age : Int, private val bankAccount: BankAccount) {
-  //Secondary Constructor
-  def this(name : String, age: Int) = this(name,  age, new SavingsAccount("12345",0.00))
 
-  private val years : String = if(age > 1) "years" else "year"
 
-  def speak() : String = {
-    if(name == "adam") {
-      s"You don't get a hello!"
-    }else {
-      s"Hello $name, you are $age $years old \n " +
-        s"You have ${bankAccount.balance} in your account."
-    }
-  }
-}
 
 object GreeterApplication extends App {
-  val name = Prompt.ask("What is your name? ")
-  val age = Prompt.ask("How old are you? ")
-  val person = new Person(name, age.toInt)
-  println(person.speak())
+  //val name = Prompt.ask("What is your name? ")
+  //val age = Prompt.ask("How old are you? ")
+  //val cashisa = new CashISASavingsAccount("45676", 0.0, 1000.00)
+  //val deposited = cashisa.deposit(1000.00)
+  //val withdrawn = deposited.withdraw(200.00)
+  //val person = new Person(name, age.toInt, withdrawn)
+  //println(person.speak())
+  val normalAccount = new CashISASavingsAccount("12334", 100.00)
+  val loyalAccountDeposited = normalAccount.deposit(300.00)
+  val loyal = new Person("Loyal customer", 22, loyalAccountDeposited)
+  Prompt.reply(loyal.speak())
 }
 
-abstract class BankAccount(accountNumber : String,
-                           val balance : Double){
-  def withdraw(amount : Double) : BankAccount
-  def deposit(amount : Double) : BankAccount
-}
 
-final class SavingsAccount(accountNumber: String,
-                           balance: Double) extends BankAccount(accountNumber, balance){
-  override def withdraw(amount: Double): BankAccount = {
-    if((balance - amount) < 0) {
-      println(s"You have insufficient funds")
-      this
-    } else {
-      val deducted = balance - amount
-      println(s"Balance after deductions: $deducted")
-      new SavingsAccount(accountNumber, deducted)
-    }
-  }
-  override def deposit(amount: Double): BankAccount = {
-    new SavingsAccount(accountNumber, balance + amount)
-  }
-}
 
-final class CashISASavingsAccount(accountNumber: String,
-                                  balance: Double) extends BankAccount(accountNumber, balance){
-  override def withdraw(amount: Double): BankAccount = {
-    println(s"You can not withdraw from this account yet")
-    this
-  }
-  override def deposit(amount: Double): BankAccount = {
-    new CashISASavingsAccount(accountNumber, balance + amount)
-  }
-}
+
+
+
 
